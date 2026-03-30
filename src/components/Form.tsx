6faa40f67a1ad0daad02bdbfdc42e0963d74ffae
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-03-30 16:29:56 -0300

    feat: implement Form component with hooks and validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
+import type { User } from "../types/User";
+import { validate } from "../utils/validate";
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const [errors, setErrors] = useState<User | null>(null);
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setErrors(null);
+
+    const data: User = {
+      name,
+      email,
+      agree,
+    };
+
+    const validateErrors = validate(data);
+
+    console.log(data, validateErrors);
+
+    if (Object.keys(validateErrors).length > 0) {
+      setErrors(validateErrors);
+      return;
+    }
+
+    setName("");
+    setEmail("");
+    setAgree(false);
+
+    alert("Obrigado por se inscrever");
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="name">
           Nome
@@ -10,7 +47,12 @@ const Form = () => {
           type="text"
           placeholder="Digite seu nome"
           className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        {errors?.name && (
+          <small className="text-xs text-red-500 mt-1">{errors?.name}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="email">
@@ -21,18 +63,31 @@ const Form = () => {
           type="email"
           placeholder="Insira seu melhor e-mail"
           className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
+        {errors?.email && (
+          <small className="text-xs text-red-500 mt-1">{errors?.email}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <a href="#" className="text-xs underline mb-2">
           Leia os termos
         </a>
         <div className="flex gap-2 items-center">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={agree}
+            onChange={(e) => setAgree(e.target.checked)}
+          />
+
           <label className="text-sm" htmlFor="agree">
             Concordo com os termos
           </label>
         </div>
+        {errors?.agree && (
+          <small className="text-xs text-red-500 mt-">{errors?.agree}</small>
+        )}
       </div>
       <button
         type="submit"
